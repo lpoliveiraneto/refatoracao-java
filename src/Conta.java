2c39e8a1f708e58f9b610478860488b44af41f37
lpoliveiraneto <leonidespires@gmail.com>
2023-01-28 19:43:37 -0300

    projeto do curso de rafatoração

---
+++ src/Conta.java
@@ -0,0 +1,18 @@
+
+public class Conta {
+	private double saldo;
+	
+	public Conta(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+
+	/*Trecho para tirar a repitação da classe banco*/
+}
